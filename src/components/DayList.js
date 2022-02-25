5130a154edc60cd96841e6517d03052f8e6a52da
Noah V <noah@noahvandenberg.com>
2022-02-24 16:28:25 -0800

    create DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+
+export default function DayList(props) {
+  const {days, day, setDay} = props
+
+  const arrDays = days.map(da => (
+    <DayListItem 
+    key={da.id} 
+    spots={da.spots} 
+    setDay={setDay} 
+    name={da.name}
+    selected={day === da.name}
+    />
+  ));
+
+  return (
+    <ul>
+      {arrDays}
+    </ul>
+  );
+}
